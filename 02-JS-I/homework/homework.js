36a7c862aac4d66abd5e645dc1c797d375f79e5b
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-01-01 13:05:00 -0500

    only eight problems left

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -161,20 +161,25 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (numero > 0) {return "Es positivo"}
+  else if (numero < 0) {return "Es negativo"}
+  else {return false}
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-}
+  str = str + "!"
+  return str
+}   
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var c = nombre + " "+ apellido
+  return c 
 }
 
 function obtenerSaludo(nombre) {
